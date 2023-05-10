347dea18862eae2d8459155e3d9db7986a4112be
lixiaoyi-12 <56015013+lixiaoyi-12@users.noreply.github.com>
2023-05-10 15:45:18 +0800

    Update fcm_threshold.sh

--- fcm_threshold.sh
+++ fcm_threshold.sh
@@ -1,14 +1,14 @@
 echo "lost in hummingbird & at least 16 songbirds"
-awk '{if($6==0&&$11==1&&$NF<=17) print$1,$2,$3}' nsuce_detail_table_summary.txt | tr " " "\t" > nsuce_lost_hummingbird_at_least_16_songbirds.txt
-wc -l nsuce_lost_hummingbird_at_least_16_songbirds.txt
+awk '{if($4==0&&$5==1&&$NF<=16) print$1,$2,$3}' ce_detail_table_summary.txt | tr " " "\t" > ce_lost_hummingbird_at_least_16_songbirds.txt
+wc -l ce_lost_hummingbird_at_least_16_songbirds.txt
 echo "lost in hummingbird & parrot & at least 16 songbirds"
-awk '{if($6==0&&$11==0&&$NF<=16) print$1,$2,$3}' nsuce_detail_table_summary.txt | tr " " "\t" > nsuce_lost_hummingbird_parrot_at_least_16_songbirds.txt
-wc -l nsuce_lost_hummingbird_parrot_at_least_16_songbirds.txt
+awk '{if($4==0&&$5==0&&$NF<=16) print$1,$2,$3}' ce_detail_table_summary.txt | tr " " "\t" > ce_lost_hummingbird_parrot_at_least_16_songbirds.txt
+wc -l ce_lost_hummingbird_parrot_at_least_16_songbirds.txt
 echo "lost in hummingbird & parrot"
-awk '{if($6==0&&$11==0&&$NF>16) print$1,$2,$3}' nsuce_detail_table_summary.txt | tr " " "\t" > nsuce_lost_hummingbird_parrot.txt
-wc -l nsuce_lost_hummingbird_parrot.txt
+awk '{if($4==0&&$5==0&&$NF>16) print$1,$2,$3}' ce_detail_table_summary.txt | tr " " "\t" > ce_lost_hummingbird_parrot.txt
+wc -l ce_lost_hummingbird_parrot.txt
 echo "lost in parrot & at least 16 songbirds"
-awk '{if($6==1&&$11==0&&$NF<=17) print$1,$2,$3}' nsuce_detail_table_summary.txt | tr " " "\t" > nsuce_lost_parrot_16_songbirds.txt
-wc -l nsuce_lost_parrot_16_songbirds.txt
+awk '{if($4==1&&$5==0&&$NF<=16) print$1,$2,$3}' ce_detail_table_summary.txt | tr " " "\t" > ce_lost_parrot_16_songbirds.txt
+wc -l ce_lost_parrot_16_songbirds.txt
 
-cat nsuce_lost_hummingbird_at_least_16_songbirds.txt nsuce_lost_hummingbird_parrot_at_least_16_songbirds.txt nsuce_lost_hummingbird_parrot.txt nsuce_lost_parrot_16_songbirds.txt > fcm.bed
+cat ce_lost_hummingbird_at_least_16_songbirds.txt ce_lost_hummingbird_parrot_at_least_16_songbirds.txt ce_lost_hummingbird_parrot.txt ce_lost_parrot_16_songbirds.txt > fcm.bed
