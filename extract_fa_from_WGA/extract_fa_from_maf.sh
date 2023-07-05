91986cf06fadcee271c936acca453680f2ae373f
lixiaoyi-12 <56015013+lixiaoyi-12@users.noreply.github.com>
2023-07-05 16:23:04 +0800

    Update extract_fa_from_maf.sh

--- extract_fa_from_WGA/extract_fa_from_maf.sh
+++ extract_fa_from_WGA/extract_fa_from_maf.sh
@@ -1,13 +1,13 @@
 After we download whole genome alignments from UCSC, we first need to transform the format from hal to maf by chromosomes<br>
 take chr1 for example<br>
-'''shell
+```shell
 hal2maf 363-avian-2020.hal 363-avian-2020_refGenome_chr1.maf --refGenome Gallus_gallus --refSequence chr1 --targetGenomes species_list
-'''
+```
 then we need to remove redundant sequence in multiple genome alignment we download, because our script does't recognize those sequence and error will occur<br>
-'''shell
+```shell
 python rm_redundant_gallus.py 363-avian-2020_refGenome_chr1.maf
-'''
+```
 then we use the following python script to extract fasta for each block<br>
-'''shell
+```shell
 python extract_fasta_from_maf_format_for_each_block.py fcm.chr1.txt 363-avian-2020_refGenome_chr1.maf
-'''
+```
