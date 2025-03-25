e28ecf7f30ec26e73808da327ee0ddf79be53047
lixiaoyi-12 <56015013+lixiaoyi-12@users.noreply.github.com>
2025-03-25 14:55:44 +0800

    Rename ggplot_bar_plot_with_error_bar.R to plot_ggplot_bar_with_error_bar.R

--- R script/ggplot_bar_plot_with_error_bar.R
+++
@@ -1,3 +0,0 @@
-library(ggplot2)
-data<-data.frame(group=c("1281","347","100","control"),mean=c(1.72,1.19,2.78,2.35),sd=c(0.17,0.08,0.24,0.16))
-ggplot(data,aes(x=group,y=mean))+geom_bar(stat='identity',fill="gray",color="black",width=0.7)+geom_errorbar(aes(ymin=mean-sd,ymax=mean+sd),width=.2,color="black")+labs(title="none",x="relative value",y="mean value")+theme_classic()+scale_y_continuous(expand = c(0,0),limits = c(0, 4))
